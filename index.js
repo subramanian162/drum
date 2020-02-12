5810b2f98935b71703d0706cf56fbfe61434b4f5
subramanian162 <46120954+subramanian162@users.noreply.github.com>
2020-02-12 18:36:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
+//Detecting Button press
 
-function testFunction(){
-  var key = this.innerHTML;
+for (var i = 0; i < document.getElementsByClassName("drum").length ; i++) {
+  document.getElementsByClassName("drum")[i].addEventListener("click",function () {
+      var key = this.innerHTML;
+      playSoundWithRespectToPressedKey(key);
+      buttonAnimation(key);
+  });
+}
 
-  if(key === "w"){
-    this.style.color = "white";
-    var audio = new Audio("tom-1.mp3");
-    audio.play();
-  }
-  else if(key === "a"){
-    this.style.color = "white";
-    var audio = new Audio("tom-2.mp3");
-    audio.play();
-  }
-  else if(key === "s"){
-    this.style.color = "white";
-    var audio = new Audio("tom-3.mp3");
-    audio.play();
-  }
-  else if(key === "d"){
-    this.style.color = "white";
-    var audio = new Audio("tom-4.mp3");
-    audio.play();
-  }
-  else if(key === "j"){
-    this.style.color = "white";
-    var audio = new Audio("snare.mp3");
-    audio.play();
-  }
-  else if(key === "k"){
-    this.style.color = "white";
-    var audio = new Audio("crash.mp3");
-    audio.play();
-  }
-  else{
-    this.style.color = "white";
-    var audio = new Audio("kick-bass.mp3");
-    audio.play();
+//Detecting KeyBoard press
+
+document.addEventListener("keydown" , function (event){
+  var pressedKey = event.key;
+  playSoundWithRespectToPressedKey(pressedKey);
+  buttonAnimation(pressedKey);
+});
+
+//Play an Audio based on the key and button pressedKey
+
+function playSoundWithRespectToPressedKey(pressedKey){
+  switch (pressedKey) {
+    case "w":
+      var audio = new Audio("tom-1.mp3");
+      audio.play();
+    break;
+    case "a":
+      var audio = new Audio("tom-2.mp3");
+      audio.play();
+    break;
+    case "s":
+      var audio = new Audio("tom-3.mp3");
+      audio.play();
+    break;
+    case "d":
+      var audio = new Audio("tom-4.mp3");
+      audio.play();
+    break;
+    case "j":
+      var audio = new Audio("snare.mp3");
+      audio.play();
+    break;
+    case "k":
+      var audio = new Audio("crash.mp3");
+      audio.play();
+    break;
+    case "l":
+      var audio = new Audio("kick-bass.mp3");
+      audio.play();
+    break;
+    default:
+     console.log("Invalid Key..\n Press a valid key");
   }
 }
 
-for (var i = 0; i < document.getElementsByClassName("drum").length ; i++) {
-  document.getElementsByClassName("drum")[i].addEventListener("click",testFunction);
+function buttonAnimation(pressedKey)
+{
+  var currentButton = document.querySelector("."+pressedKey);
+
+  //Now it show the Button pressed view
+  currentButton.classList.add("pressed");
+
+  //After 1 sec button will be in normal view
+  setTimeout(function () {
+    currentButton.classList.remove("pressed");
+  }, 100);
+
 }
